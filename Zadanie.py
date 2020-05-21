cc30c0e192bf9065feb731318782379071070938
Filip Gabrys <el.filberto@gmail.com>
2020-05-21 22:15:28 +0200

    Add functions *get_movies* and *get_series*

--- Zadanie.py
+++ Zadanie.py
@@ -1,3 +1,5 @@
+import itertools
+
 class Movie:
     def __init__(self, title, year, typ):
         self.title = title
@@ -13,6 +15,7 @@ class Movie:
     def __str__(self):
         return f"{self.title}, {self.year}"
 
+
 class Series(Movie):
     def __init__(self, season, episode, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -25,13 +28,37 @@ class Series(Movie):
 
 Pulp = Movie(title = "Pulp Fiction", year = "1994", typ = "kryminał")
 Cher = Series(title = "Cherobyl", year = "2019", typ = "Dramat", season = "01", episode = "03")
+Aven = Movie(title= "Avengers", year = "2012", typ = "Sci-Fi")
+Fri = Series(title = "Friends", year = "1999", typ = "komedia", season = "06", episode = "04")
 
 base_list = []
 base_list.append(Pulp)
 base_list.append(Cher)
+base_list.append(Aven)
+base_list.append(Fri)
 
-for i in base_list:
-    print(i)
+list_to_sort=[]
+
+def get_movies(a):
+    list_to_sort = [i for i in a if i.__class__.__name__ == 'Movie']
+    list_to_sort = sorted(list_to_sort, key=lambda Movie: Movie.title)
+    for i in list_to_sort:
+        print(i)
+
+        
+
+def get_series(a):
+    list_to_sort = [i for i in a if i.__class__.__name__ == 'Series']
+    list_to_sort = sorted(list_to_sort, key=lambda Series: Series.title)
+    for i in list_to_sort:
+        print(i)
+
+
+get_movies(base_list)
+get_series(base_list)
+    
+
+
 
 
 
