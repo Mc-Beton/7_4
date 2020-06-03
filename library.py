024eb37967f6e72832ae01dc1c9906dfa82de8bf
Filip Gabrys <el.filberto@gmail.com>
2020-06-03 22:27:45 +0200

    Update MRO of Library

--- library.py
+++ library.py
@@ -1,29 +1,25 @@
 from movie import Movie
 from series import Series
 
-class Library:
-    def __init__(self):
+base_list = []
+class Library(Series, Movie):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        #variables
-        self.base_list = []
+        
 
     def add_movie(self):
-        i = input("What is the title ")
-        j = input("What year was it made ")
+        i = input("What is the title? ")
+        j = input("What year was it made? ")
         k = input("What type of movie is it? ")
-        self.base_list.append(Movie(title = i, year = j, typ = k))
+        base_list.append(Movie(title = i, year = j, typ = k))
     
     def add_series(self):
-        i = input("What is the title ")
-        j = input("What year was it made ")
+        i = input("What is the title? ")
+        j = input("What year was it made? ")
         k = input("What type of movie is it? ")
         l = int(input("How many seasons does it have? "))
         for s in range(l):
             m = int(input(f"How many episodes does season {s+1} have? "))
             for e in range(m):
-                self.base_list.append(Series(title = i, year = j, typ = k, season = str(s+1), episode = str(e+1)))
-
-    @property
-    def base_list_content(self):
-        return self.base_list
-    
+                base_list.append(Series(title = i, year = j, typ = k, season = str(s+1), episode = str(e+1)))
